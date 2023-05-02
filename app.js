9278a22c846aaf106588b735906027e817c87210
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-02 22:28:00 +0800

    Implemented mongoose and resolved the type error

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 // var->let->const (still modifiable)
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const date = require(__dirname + "/date.js");
 
@@ -14,13 +16,36 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB', {useNewUrlParser : true});
+
+const itemSchema = new Schema({
+ _id : Number,
+ name:  String
+});
+const Item = mongoose.model("Item", itemSchema);
+const item1 = new Item({
+ _id : 1,
+ name:"Welcome to your ToDo List!"});
+const item2 = new Item({
+ _id : 2,
+ name:"Hit the + button to add new item."});
+const item3 = new Item({
+ _id :3,
+ name:"<-- Hit this to delete an item."});
+
+const defaultItems = [item1, item2, item3];
+Item.insertMany(defaultItems)
+  .then(function () {
+    console.log("Successfully saved defult items to DB");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
 app.get("/", function(req, res) {
-// when only one fun
-  // let day = date();
-// when multiple fun
   let day = date.getDay();
   res.render("list", {
-    listTitle: day,
+    listTitle: "Today",
     newListItems: items
   });
 });
@@ -28,7 +53,7 @@ app.get("/", function(req, res) {
 app.post("/",function(req,res){
   let item = req.body.newItem;
   // to check whats posted
-    console.log(req.body);
+    console.log("home " + req.body.list);
   if(req.body.list === "Work List"){
     workItems.push(item);
     res.redirect("/work");
